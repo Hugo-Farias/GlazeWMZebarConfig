225608620dd194e518ef92645db99be4efb488f6
Hugo-Farias <hg80rod@gmail.com>
2025-09-21 23:36:31 -0300

    add workspace variable so reorganize workspace returns to previous workspace after done

--- glazewm/ReorganizeWorkspaces.ps1
+++ glazewm/ReorganizeWorkspaces.ps1
@@ -10,28 +10,43 @@ public static extern IntPtr GetConsoleWindow();
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
 
+function Get-GlazeCurrentWorkspace {
+  $json = glazewm query monitors | ConvertFrom-Json
+  $json.data.monitors |
+  Where-Object { $_.hasFocus } |
+  ForEach-Object {
+    $_.children | Where-Object { $_.hasFocus } | Select-Object -ExpandProperty name
+  }
+}
+
 $console = [Console.Window]::GetConsoleWindow()
 
 # 0 hide
 [Console.Window]::ShowWindow($console, 0) | Out-Null
 
-for ($i = 1; $i -le 9; $i++) {
+$workspace = Get-GlazeCurrentWorkspace
+
+for ($i = 1; $i -le 12; $i++) {
     glazewm command focus --workspace $i
     glazewm command set-tiling-direction horizontal
     glazewm command move-workspace --direction right
+    # Start-Sleep -Milliseconds 50
 }
 
 # # Move workspaces 10 through 12 to the left
-for ($i = 10; $i -le 12; $i++) {
-    glazewm command focus --workspace $i
-    glazewm command set-tiling-direction horizontal
-    # glazewm command move-workspace --direction left
-}
+# for ($i = 10; $i -le 12; $i++) {
+#     glazewm command focus --workspace $i
+#     glazewm command set-tiling-direction horizontal
+#     # glazewm command move-workspace --direction left
+# }
 
-glazewm command focus --workspace 11
+# glazewm command focus --workspace 11
 # glazewm command focus --workspace 10
 
-glazewm command focus --workspace 1
+glazewm command focus --workspace 13
+glazewm command set-tiling-direction horizontal
+glazewm command move-workspace --direction left
+glazewm command focus --workspace $workspace
 
 # pause
 # Start-Sleep -Seconds 1
@@ -39,8 +54,8 @@ glazewm command focus --workspace 1
 SCHTASKS.EXE /END /TN \TrustedApps\SmoothScroll
 TASKKILL /F /IM SmoothScroll.exe T
 timeout 1
-REM SCHTASKS.EXE /RUN /TN "\TrustedApps\SmoothScroll Registration"
-REM SCHTASKS.EXE /RUN /TN "\TrustedApps\SmoothScroll Registration"
+# REM SCHTASKS.EXE /RUN /TN "\TrustedApps\SmoothScroll Registration"
+# REM SCHTASKS.EXE /RUN /TN "\TrustedApps\SmoothScroll Registration"
 SCHTASKS.EXE /RUN /TN \TrustedApps\SmoothScroll
 
 
