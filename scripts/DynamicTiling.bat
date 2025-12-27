eea179ff9c04f080878c004dac11176a4991d46a
Hugo-Farias <hg80rod@gmail.com>
2025-12-27 10:14:52 -0300

    change main brave window to always open in workspace 1

--- scripts/DynamicTiling.bat
+++ scripts/DynamicTiling.bat
@@ -1,13 +1,15 @@
-@echo off
+REM @echo off
 setlocal enabledelayedexpansion
 
 :: Example JSON
 REM for /f "delims=" %%A in ('glazewm query focused') do set "json=%%A"
-for /f "delims=" %%A in ('glazewm query focused ^| rg "\"width\"\:(\d+)" -or "$1" -') do set "W=%%A"
+REM for /f "delims=" %%A in ('glazewm query focused ^| rg "\"width\"\:(\d+)" -or "$1" -') do set "W=%%A"
+for /f "delims=" %%A in ('glazewm query focused ^| rg "\"x\"\:(\d+)\," -or "$1" -') do set "W=%%A"
 
-if 1900 GTR !W! (
+REM echo !W!
+
+if !W! GTR 10 (
   glazewm command set-tiling-direction vertical
 ) else (
   glazewm command set-tiling-direction horizontal
 )
- 
